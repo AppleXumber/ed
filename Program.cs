5b636d543d0270566bbfdc9b86eb523fcb5dd5f0
Wayron <wayron.jhonny@gmail.com>
2025-04-15 21:56:54 -0300

    Listas

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace ed
     {
         static void Main(string[] args)
         {
+            Lista lista = new Lista(10);
+            List<Char> list = new List<Char>();
+            list.Add('W');
+            lista.Inserir('W');
+            list.Add('a');
+            lista.Inserir('a');
+            list.Add('y');
+            lista.Inserir('y');
+            list.Add('r');
+            lista.Inserir('r');
+            list.Add('o');
+            lista.Inserir('o');
+            list.Add('n');
+            lista.Inserir('n');
+            list.Insert(2, 'e');
+            lista.InserirNaPosicao(2, 'e');
+            list.RemoveAt(2);
+            lista.Remover(3);
+            lista.Exibir();
+            Console.WriteLine(string.Join(" ", list));
         }
     }
 }
